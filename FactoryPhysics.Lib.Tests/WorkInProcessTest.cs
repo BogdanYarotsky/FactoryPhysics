006fcdba77d63bae88d2169236cff7d3bfa3f549
byar <byarotsky@gmail.com>
2024-07-22 20:23:24 +0200

    added tests

---
+++ FactoryPhysics.Lib.Tests/WorkInProcessTest.cs
@@ -0,0 +1,18 @@
+using FactoryPhysics.Lib;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactoryPhysics.Lib.Tests;
+
+[TestClass]
+[TestSubject(typeof(WorkInProcess))]
+public class WorkInProcessTest
+{
+
+    [TestMethod]
+    public void GetExpectedWip()
+    {
+        var wip = WorkInProcess.From(ct: new(2), th: new(0.4));
+        Assert.AreEqual(0.8, wip.Jobs);
+    }
+}
